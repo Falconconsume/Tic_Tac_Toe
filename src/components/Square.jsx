07e1295e4b8479d2ad846e786a44ae41b9fe7f83
Simply Done <olko4455@gmail.com>
2024-02-01 10:10:41 +0200

    first commit

---
+++ src/components/Square.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+
+export default function Square({value, onSquareClick}) {
+
+  return (
+    <div>
+      <button onClick={onSquareClick} className="square">{value}</button>
+    </div>
+  );
+}
